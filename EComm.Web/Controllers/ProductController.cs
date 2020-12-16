16e7921c933e017b6b1ed200e92ca6f21be85868
Robert Wilk <robertwilk916@gmail.com>
2020-12-16 13:43:10 -0800

    Added product controller and details view

---
+++ EComm.Web/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using EComm.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EComm.Web.Controllers
+{
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly IRepository _repo;
+
+        public ProductController(IRepository repo)
+            => _repo = repo;
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _repo.GetProduct(id, true);
+            return View(product);
+        }
+    } 
+}
